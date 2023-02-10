c5f493c39cc90430f806801afdf9c55fad6ac9ff
saifulemon <463saiful0@gmail.com>
2023-02-10 23:24:19 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import TopBar from "./components/TopBar";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Policy from "./components/Policy";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Router>
       <TopBar />
+      <NavBar />
       <Switch>
         <Route path="/about" component={About} exact/>
         <Route path="/contact" component={Contact} exact/>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, Image, Nav, Navbar } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
+const NavBar = () => {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Brand>
+            <Image src="images/logo.png" style={{width:"300px", height:"120px"}}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/cart">
+                <Nav.Link>Cart</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default NavBar;
